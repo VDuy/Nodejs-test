a17577f555b52c9c471b78783ea0ea2b68d1781a
Vũ Duy <ducduy23899@gmail.com>
2021-08-24 17:36:42 +0700

    excel examples

--- node_server_base/src/app.js
+++ node_server_base/src/app.js
@@ -14,18 +14,22 @@ const indexRouter = require('./routes/indexRoutes');
 const userRouter = require('./routes/userRouter');
 
 const response = require('./common/response');
-
+const excel = require("./excel/indexExcel");
+excel;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors());
 
-//app.use('/', indexRouter);
+app.use('/', indexRouter);
 app.use('/user', userRouter);
 
 indexRouter(app);
 configView(app);
+
+
+
 
 const PORT = process.env.PORT || 8082;
 app.listen(PORT, () => {
--- node_server_base/src/routes/indexRoutes.js
+++ node_server_base/src/routes/indexRoutes.js
@@ -3,10 +3,10 @@ const express = require("express");
 const Indexs = require('../models/index')
 const indexController = require('../controller/indexController');
 // const response = require('../common/response');
-
 const router = express.Router();
+
 const initRouter = (app) => {
-    router.get("/index", indexController.apps);
+    router.get("/", indexController.apps);
 }
 
 module.exports = initRouter;
