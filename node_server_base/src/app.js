17444d3ac18a9f19b681e2821c219ccf4147b651
Vũ Duy <ducduy23899@gmail.com>
2021-08-26 11:19:33 +0700

    remove excel

--- node_server_base/src/app.js
+++ node_server_base/src/app.js
@@ -14,8 +14,7 @@ const indexRouter = require('./routes/indexRoutes');
 const userRouter = require('./routes/userRouter');
 
 const response = require('./common/response');
-const excel = require("./excel/indexExcel");
-excel;
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
