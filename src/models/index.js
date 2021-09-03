0538eddc058d6646a13639c655ae9611c3500c07
Vũ Duy <ducduy23899@gmail.com>
2021-09-03 21:20:03 +0700

    change folder

--- src/models/index.js
+++ src/models/index.js
@@ -7,11 +7,9 @@ const sequelize = require('../config/env');
 var db = {};
 
 
-// fs
-//     .readdirSync(__dirname)
-//     .filter(file => {
-//         return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
-//     })
+fs.readdirSync(__dirname).filter(file => {
+        return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
+    })
 //     .forEach(file => {
 //         const model = sequelize.import(path.join(__dirname, file));
 //         db[model.name] = model;
