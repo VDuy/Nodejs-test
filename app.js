3dcf8e5c58eeead5ecb8be95c5dac30c267e824d
Vũ Duy <ducduy23899@gmail.com>
2021-09-28 12:18:56 +0700

    fix

--- app.js
+++ app.js
@@ -1,41 +1,38 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const { dotenv } = require('dotenv').config();
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const express = require('express');
 
-var app = express();
+const path = require('path');
+const cors = require("cors");
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const db = require('./models');
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+const response = require('./common/response');
+const viewEngine = require("./config/viewEngine");
+const initRoutes = require('./routes/indexRoutes');
+const initusersRoutes = require('./routes/userRoutes');
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(cors());
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+viewEngine(app);
+initRoutes(app);
+initusersRoutes(app);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+const PORT = process.env.PORT;
+app.listen(PORT, (err) => {
+  if (err) {
+    console.log(`error` + err);
+  } else {
+    console.log(`API is running at port : ${PORT}`);
+  }
+})
 
 module.exports = app;
